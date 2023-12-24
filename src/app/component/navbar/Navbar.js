7afa6ccd7c1abed2b2f08757c37abd879951b380
JackOwlTV <contact@dylanvivant.com>
2023-12-25 00:27:58 +0100

    Add navbar static
    Adding the navbar without animations.

---
+++ src/app/component/navbar/Navbar.js
@@ -0,0 +1,89 @@
+'use client'
+import React from 'react'
+import Link from 'next/link';
+
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+
+import './style.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <Link href={'/'}>
+                <div className="logo">
+                    <span className="logo">
+                        Restary
+                    </span>
+                </div>
+            </Link>
+
+            <ul id='links-pc' className="nav-links">
+                <li>
+                    <Link href="#reserve">
+                        <span>RESERVE A TABLE</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/menu">
+                        <span>BROWSE MENU</span>
+                    </Link>
+                </li>
+            </ul>
+
+            <ul id='social-pc' className="social-links">
+
+                <li>
+                    <Link href="#">
+                        <AiOutlineFacebook className="social-icon" />
+                    </Link>
+
+                </li>
+                <li>
+                    <Link href="#">
+                        <AiOutlineInstagram className="social-icon" />
+                    </Link>
+                </li>
+            </ul>
+
+            <details open={"open"} >
+                <summary>
+                    <AiOutlineMenu className="menu-icon" />
+                    <AiOutlineClose className="close-icon" />
+                </summary>
+                <div className="menu-mobile">
+                    <ul className="nav-links">
+                        <li>
+                            <Link href="#reserve">
+                                <span>RESERVE A TABLE</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/menu">
+                                <span>BROWSE MENU</span>
+                            </Link>
+                        </li>
+                    </ul>
+
+                    <ul className="social-links">
+
+                        <li>
+                            <Link href="#">
+                                <AiOutlineFacebook className="social-icon" />
+                            </Link>
+
+                        </li>
+                        <li>
+                            <Link href="#">
+                                <AiOutlineInstagram className="social-icon" />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </details>
+        </nav>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
