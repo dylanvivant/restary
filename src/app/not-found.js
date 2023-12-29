6b1a01b14d4508ffd407423d92af01b84a7fe55a
JackOwlTV <contact@dylanvivant.com>
2023-12-29 13:53:51 +0100

    Add not found page and start responsive

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
+import Navbar from './component/navbar/Navbar'
+import Footer from './component/footer/Footer'
+
+import './not-found.css'
+
 export default function NotFound() {
     return (
-        <div>not-found</div>
+        <main>
+            <Navbar />
+            <section >
+                <div className="not-found">
+                    <h1>404 - Not found</h1>
+                </div>
+                <div className="error">
+                    <p>You didn't break anything, but we cannot find what you're looking for.</p>
+                    <div className="back">
+                        <a href="/">Back to homepage</a>
+                    </div>
+                </div>
+
+            </section>
+            <Footer />
+        </main>
     )
 }
